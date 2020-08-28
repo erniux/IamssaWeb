b4b37f34053929e208098686c7884934e57bf398
erniux <erna.tercero@hotmail.com>
2020-08-28 11:38:03 -0500

    proyectos

--- app/controllers/proyectos_controller.rb
+++ app/controllers/proyectos_controller.rb
@@ -1,21 +1,58 @@
 class ProyectosController < ApplicationController
+	before_action :set_proyecto, only: [:show, :edit, :update, :destroy]
 
 	def index
-		@proyectos = Proyecto.all
+		if params[:term].present?
+      		@proyectos = Proyecto.search_by_full_proyecto(params[:term]).order(nombre_proyecto: 'ASC').page(params[:page])
+    	elsif  
+      		@proyectos = Proyecto.order(:nombre_proyecto).order(nombre_proyecto: 'ASC').page(params[:page])
+    	end 
 	end 
 
+	def show
+  	end
+
+  	def new
+    	@proyecto = Proyecto.new
+  	end
+
+  	def edit
+  	end
+      
 	def create
 		@proyecto = Proyecto.new(proyecto_params)
 		if @proyecto.save
 			render json: @proyecto, status: :created
+			redirect_to tipo_servicios_path, notice: 'Registro creado con éxito.'
 		else
+			render :new
 			render json: @proyecto.errors.full_messages, status: :unprocessable_entity
 		end
 	end
 
+	def update
+    	if @proyecto.update(proyecto_params)
+      		redirect_to proyectos_path, notice: 'Registro actualizado con éxito.'
+    	else
+      		render :edit
+    	end
+
+  	end
+
+  	def destroy
+    	@proyecto.destroy
+    	redirect_to proyectos_url, notice: 'Registro eliminado con éxito.'
+  	end
+
 	private 
+
+	def set_proyecto
+      @proyecto = Proyecto.find(params[:id])
+    end
 
 	def proyecto_params
 		params.require(:proyecto).permit(:id, :nombre_proyecto, :empresa, :responsable, :comentarios)
 	end
+
+
 end
--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,7 +1,10 @@
 class Proyecto < ApplicationRecord
+	include PgSearch::Model
 	 
- 	 
-
 	validates :nombre_proyecto, presence: true
 	validates :nombre_proyecto, uniqueness: true
+
+	 pg_search_scope :search_by_full_proyecto,  against: [:nombre_proyecto ],	
+											using:   {tsearch: { prefix: true }}		
+
 end
